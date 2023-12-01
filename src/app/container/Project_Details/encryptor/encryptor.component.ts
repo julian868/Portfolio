a7fbb52cd5782a782af9997ef564ff17c4ecddfe
Julian <julian94ellis@gmail.com>
2023-12-01 16:08:41 -0500

    Remove console log

--- src/app/container/Project_Details/encryptor/encryptor.component.ts
+++ src/app/container/Project_Details/encryptor/encryptor.component.ts
@@ -28,7 +28,6 @@ export class EncryptorComponent {
     if (this.inputPlainText != "") {
       this.http.get<any>(`https://vercel-test-ashen-phi.vercel.app/encryption/encrypt/${this.inputPlainText}`)
         .subscribe((res) => {
-          console.log(res);
           this.output = res.cipherText;
           this.outputTextHeader = 'Encrypted Cyphertext:';
         })
